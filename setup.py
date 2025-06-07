f3a5a976c37720fc3269c517b39cf5cfd6f0b5d9
ganesanluna <ganesanluna@yahoo.in>
2025-06-07 21:58:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,14 @@ setup(
     author="Ganesan Selvaraj",
     author_email="ganesanluna@yahoo.in",
     description="Robot Framework library for Android ADB interaction",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/ganesanluna/ADBLibrary",
+    project_urls={
+        "Documentation": "https://github.com/ganesanluna/ADBLibrary#readme",
+        "Source": "https://github.com/ganesanluna/ADBLibrary",
+        "Tracker": "https://github.com/ganesanluna/ADBLibrary/issues",
+    },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
@@ -17,9 +22,10 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Framework :: Robot Framework",
+        "Framework :: Robot Framework :: Library",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    keywords=["robotframework", "adb", "android", "automation", "testing"],
     python_requires='>=3.6',
 )
-
